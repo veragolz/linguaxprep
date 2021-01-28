501ee47076abcacaca9f0a37f202ca475851b397
Vera G <vera.golz98@web.de>
2021-01-28 15:12:07 +0100

    new commit

---
+++ de.unidue.ltl.emptySkill/src/main/java/com/amazon/customskill/DBConnection.java
@@ -0,0 +1,29 @@
+package com.amazon.customskill;
+
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class DBConnection {
+
+	static String DBName = "linguaxprepdb.db";
+	private static Connection con = null;
+/*
+ * establishing the connection with the SQLite database 
+ * */
+	public static Connection getConnection() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			try {
+				con = DriverManager.getConnection("jdbc:sqlite::resource:" + 
+				           DBConnection.class.getClassLoader().getResource(DBName));
+				} catch (SQLException ex) {
+				System.out.println("Failed to create the database connection.");
+				ex.printStackTrace();
+			}
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		return con;
+	}
+
+}
